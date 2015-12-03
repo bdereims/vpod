6bca9da85da41497ade18c48e97b67eb2de65fac
Brice Dereims <bdereims@gmail.com>
2015-12-03 23:45:44 +0100

    wait message updated

--- esx01/gen_welcome.sh
+++ esx01/gen_welcome.sh
@@ -2,14 +2,29 @@
 #Welcome Message                                                                                                                                                                                                        
 
 ExtIP="<unset>"
+Waiter=1
 while [ $ExtIP = "<unset>" ]
 do
-	echo "Retreiving IP of vPodRouter..."
 	ExtIP=$(vim-cmd vmsvc/get.guest $(vim-cmd vmsvc/getallvms | grep "vPodRouter" | awk '{print $1}') | grep -m 1 "ipAddress" | sed -e 's/^.*=//' -e 's/[",\ ,\,]//g')
-	cp /vmfs/volumes/dsLocalESX01/welcome.orig /etc/vmware/welcome
-	sed 's/####/'$ExtIP'/' -i /etc/vmware/welcome
-	kill $(ps | grep dcui | awk '{print $1}')	
-	sleep 3 
+	
+	if [ $ExtIP != "<unset>" ]; then
+		break 
+	fi
+
+	cp /vmfs/volumes/dsLocalESX01/welcome.wait /etc/vmware/welcome
+	cat /vmfs/volumes/dsLocalESX01/wait.${Waiter} >> /etc/vmware/welcome
+	kill $(ps | grep dcui | awk '{print $1}') 
+
+	Waiter=$(expr ${Waiter} + 1 )
+	if [ ${Waiter} -gt 6 ]; then
+		Waiter=1
+	fi
+
+	sleep 1 
 done
+
+cp /vmfs/volumes/dsLocalESX01/welcome.orig /etc/vmware/welcome
+sed 's/####/'$ExtIP'/' -i /etc/vmware/welcome
+kill $(ps | grep dcui | awk '{print $1}')	
 
 exit 0
--- push-conf.sh
+++ push-conf.sh
@@ -1,4 +1,6 @@
-scp esx01/welcome.orig esx01:/vmfs/volumes/dsLocalESX01/welcome.orig
+scp esx01/welcome.* esx01:/vmfs/volumes/dsLocalESX01/.
+scp esx01/wait.* esx01:/vmfs/volumes/dsLocalESX01/.
+scp esx01/gen_welcome.sh esx01:/vmfs/volumes/dsLocalESX01/.
 ssh esx01 "/vmfs/volumes/dsLocalESX01/gen_welcome.sh"
 
 scp esx02/welcome.orig esx02:/vmfs/volumes/dsLocalESX02/welcome.orig
