b5f18a9a60cc438d8beaa5b6275ffdae4d8af736
Brice Dereims <bdereims@gmail.com>
2015-12-04 10:35:30 +0100

    add vic scripts

---
+++ vic/create-vch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# $1 : name of VCH
+# $2 : the datastore
+
+export PATH=$PATH:/opt/vmware/vic/bonneville
+
+cd /opt/vmware/vic/bonneville
+./install-linux -user=Administrator@vsphere.local -target=172.16.66.2 '-passwd=VMware1!' -name=$1 -datastore=$2 -datacenter=Datacenter -cluster=Cluster -ceip=disable -memoryMB=512 -externalNetwork=vPodNetwork
