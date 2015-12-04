6bd50beda6308c0f0bd8eef08af43fecbad6acce
Brice Dereims <bdereims@gmail.com>
2015-12-04 10:23:53 +0100

    files from etc

--- vpodrouter/backup.sh
+++ vpodrouter/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /
-tar cvzf /data/vpod/vpodrouter/vpod-conf.tgz etc/bind etc/dhcp etc/rc.local etc/sysctl.conf etc/network/interfaces etc/issue* etc/motd
+tar cvzf /data/vpod/vpodrouter/vpod-conf.tgz etc/bind etc/dhcp etc/rc.local etc/sysctl.conf etc/network/interfaces etc/issue* etc/motd etc/ntp.conf
 cd -
